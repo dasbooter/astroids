8fdc17e970633aaddedf46588222a68c64b8c783
Rasputin <dasturboboot@gmail.com>
2024-08-21 14:53:14 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+pygame.init
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+if __name__ == "__main__":
+    main()
